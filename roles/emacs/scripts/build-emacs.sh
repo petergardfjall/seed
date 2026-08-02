c644c1c4015749f50264090434df869eb86f997b
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2026-08-02 11:15:00 +0200

    emacs: 30.2 -> 31.0.91

--- roles/emacs/scripts/build-emacs.sh
+++ roles/emacs/scripts/build-emacs.sh
@@ -57,8 +57,8 @@ log "commit to build: ${rev_commit}"
 git checkout -B ${revision}-branch ${rev_commit}
 log "building on branch ${revision}-branch (see ${build_log}) ..."
 
-export CC="/usr/bin/gcc-15"
-export CXX="/usr/bin/g++-15"
+export CC="/usr/bin/gcc"
+export CXX="/usr/bin/g++"
 git clean -dxf               >> ${build_log} 2>&1
 ./autogen.sh                 >> ${build_log} 2>&1
 ./configure ${compile_flags} >> ${build_log} 2>&1
